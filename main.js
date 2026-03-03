3e577b34aa9bc5cb6ed6d6cdf6fb50c4039aeb16
SeanEdwardSmith <sean.edward.smith@outlook.com>
2026-03-03 11:14:46 +0000

    navbar scroll transform implemented

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ const mobileNav = document.querySelector('.mobile-nav');
 navButton.addEventListener('click', () => {
     mobileNav.classList.toggle('active');
 });
+
+const navbar = document.querySelector('.navbar');
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > lastScrollY) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+
+    lastScrollY = currentScrollY;
+});
